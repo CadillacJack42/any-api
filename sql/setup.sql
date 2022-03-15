2dc0655f81472f2c117adc99b2030203201cb0fd
James Keicher <jamesdkeicher@gmail.com>
2022-03-15 14:46:17 -0700

    feat: setup Band class object, fix 404 error (Syntax errors)

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,6 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP IF TABLE EXISTS bands;
+DROP TABLE IF EXISTS bands;
 
 CREATE TABLE bands (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
